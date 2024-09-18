e5f639bfc0084c493df5b3342d90cae43bba2438
Diego Balderrama <diegowrhasta@gmail.com>
2024-09-18 10:48:40 -0400

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Container, Stack, Typography } from '@mui/material'
+
+import './App.css'
+import { JavaScriptLogo } from './JavaScriptLogo'
+import { Start } from './Start'
+
+function App () {
+  return (
+    <main>
+      <Container maxWidth='sm'>
+        <Stack
+          direction='row'
+          gap={2}
+          alignItems='center'
+          justifyContent='center'
+        >
+          <JavaScriptLogo></JavaScriptLogo>
+          <Typography variant='h2' component='h1'>
+            JavaScript Quizz
+          </Typography>
+        </Stack>
+
+        <Start />
+      </Container>
+    </main>
+  )
+}
+
+export default App
---
+++ src/Start.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@mui/material'
+
+function Start () {
+  return (
+    <Button onClick={() => {}} variant='contained'>
+      Start!
+    </Button>
+  )
+}
+
+export { Start }
