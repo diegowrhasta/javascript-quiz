e3d188162751ee7823bdb1c67dd5eb4eb93fd4f5
Diego Balderrama <diegowrhasta@gmail.com>
2024-09-18 11:09:18 -0400

    Adds zustand and initial baseline for code [INCOMPLETE]

---
+++ src/store/types.d.ts
@@ -0,0 +1,11 @@
+interface Question {
+  id: number
+  question: string
+  code: string
+  answers: string[]
+  correctAnswer: number
+  userSelectedAnswer?: number
+  isCorrectUserAnswer?: boolean
+}
+
+export { Question }
